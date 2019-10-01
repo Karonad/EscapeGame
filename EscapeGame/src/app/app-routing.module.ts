145825902769ffa79b115101846b33157d7d2acb
Quentin <fle.quentin@gmail.com>
2019-10-01 11:42:45 +0200

    Routing

--- EscapeGame/src/app/app-routing.module.ts
+++ EscapeGame/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PuzzleCalculationComponent } from './puzzle/puzzle-calculation/puzzle-calculation.component';
+import { PuzzleJarComponent } from './puzzle/puzzle-jar/puzzle-jar.component';
+import { PuzzleSquareComponent } from './puzzle/puzzle-square/puzzle-square.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'puzzle/puzzle-calculation', component: PuzzleCalculationComponent },
+  { path: 'puzzle/puzzle-square', component: PuzzleSquareComponent },
+  { path: 'puzzle/puzzle-jar', component: PuzzleJarComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
